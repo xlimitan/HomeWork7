0bab2192f926c3cb88bb9f0075c990ccddb87e80
xlimitan <xlimitan@yandex.ru>
2023-02-15 13:04:03 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,46 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
+        task8();
+    }
+
+    public static void task1 () {
+        System.out.println("Задача 1");
+        // Пишем код для задачи 1
+    }
+
+    public static void task2 () {
+        System.out.println("Задача 2");
+        // Пишем код для задачи 2
+    }
+    public static void task3 () {
+        System.out.println("Задача 3");
+        // Пишем код для задачи 1
+    }
+    public static void task4 () {
+        System.out.println("Задача 4");
+        // Пишем код для задачи 1
+    }
+    public static void task5 () {
+        System.out.println("Задача 5");
+        // Пишем код для задачи 1
+    }
+    public static void task6 () {
+        System.out.println("Задача 6");
+        // Пишем код для задачи 1
+    }
+    public static void task7 () {
+        System.out.println("Задача 7");
+        // Пишем код для задачи 1
+    }
+    public static void task8 () {
+        System.out.println("Задача 8");
+        // Пишем код для задачи 1
+    }
+}
